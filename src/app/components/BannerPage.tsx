3ee3e375a5807e5587530f28c9d2735f818d7792
Aarón Mendoza Sianquez <poppingpleace2@gmail.com>
2026-05-03 13:01:58 -0500

    commit 0.3

--- src/app/components/BannerPage.tsx
+++ src/app/components/BannerPage.tsx
@@ -20,7 +20,7 @@ export default function BannerPage() {
               preload="auto"
               className="w-full h-full object-cover"
             >
-              <source src="/video/banner.mp4" type="video/mp4" />
+              <source src="/video/banner.webm" type="video/mp4" />
             </video>
 
             {/* Gradiente izquierdo para legibilidad del texto */}
